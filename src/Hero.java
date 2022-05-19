f360ff2f9b6852d6b3f42668b8b464e4f93d4e98
ShadowFirewook <shadowfirewook@gmail.com>
2022-05-19 20:20:31 +0300

    Initial commit

---
+++ src/Hero.java
@@ -0,0 +1,10 @@
+public abstract class Hero implements HavingSuperAbility {
+    public int HP;
+    public int DMG;
+    String superAbilityType;
+
+    @Override
+    public void applySuperAbility() {
+
+    }
+}
