009195ac9df26db4f0eb8a51728ac598eda500fa
ShadowFirewook <104684219+ShadowFirewook@users.noreply.github.com>
2022-05-19 17:21:59 +0300

    Update Warrior.java

--- src/Warrior.java
+++ src/Warrior.java
@@ -1,6 +1,6 @@
 public class Warrior extends Hero{
     @Override
     public void applySuperAbility() {
-        System.out.println("Воин применил суперспособность : Критический урон");
+        System.out.println("Р’РѕРёРЅ РїСЂРёРјРµРЅРёР» СЃСѓРїРµСЂСЃРїРѕСЃРѕР±РЅРѕСЃС‚СЊ : РљСЂРёС‚РёС‡РµСЃРєРёР№ СѓСЂРѕРЅ");
     }
 }
